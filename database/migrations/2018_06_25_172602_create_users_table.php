a20e918c06c53c1e8ee98bad344795ec71cc735c
Slava Pleshkov <mail@slavapleshkov.com>
2018-08-27 15:49:01 +0300

    v-0-2-5

--- database/migrations/2018_06_25_172602_create_users_table.php
+++ database/migrations/2018_06_25_172602_create_users_table.php
@@ -17,6 +17,7 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('email')->nullable();
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password')->nullable();
             $table->integer('github_id')->unique()->nullable();
             $table->integer('google_id')->unique()->nullable();
